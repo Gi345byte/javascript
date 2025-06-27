b06d8a23b4ff3874d201da9df51a1019eb9c8fc6
Gi345byte <113916871+Gi345byte@users.noreply.github.com>
2025-06-27 19:54:07 +0530

    Initial commit

---
+++ foreach.js
@@ -0,0 +1,37 @@
+// var l=[1,2,3,4,5];
+// l.forEach(function(e){
+//     console.log(`element at index ${e} is : ${}`);
+// });
+
+
+// map
+// var r=l.map(function(e){
+//     return e;
+// });
+// console.log(r);
+
+// filter 
+// var even=l.filter(function(e){
+
+//     return e%2==0;
+// });
+// console.log(even);
+
+// var odd=l.filter(function(e){
+
+//     return e%2!=0;
+// });
+// console.log(odd);
+
+// let a=5;
+// var fact = a.reduce(function(acc,element){
+//    return acc * element;
+// });
+// console.log(fact);
+
+
+// var y=l.
+
+
+
+
---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('My name is Swapnil Marke , i am 21 year old , living in the heaven named sahyadri')
+var s=24;
+{
+    document.write(s + " hellow");
+}
+document.writeln("heyy");
+console.log()
